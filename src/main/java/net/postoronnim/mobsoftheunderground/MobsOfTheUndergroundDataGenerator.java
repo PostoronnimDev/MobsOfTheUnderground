82fb55373507dc7d6026e98c629738ffc43af2f6
postoronnimdev <postoronnimdev@gmail.com>
2025-05-10 07:38:36 +0300

    project setup

---
+++ src/main/java/net/postoronnim/mobsoftheunderground/MobsOfTheUndergroundDataGenerator.java
@@ -0,0 +1,11 @@
+package net.postoronnim.mobsoftheunderground;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MobsOfTheUndergroundDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
